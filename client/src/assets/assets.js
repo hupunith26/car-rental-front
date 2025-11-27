80993122ff103c2bb1ec5474e7e8a7edc682c464
hupunith26 <punithhu273@gmail.com>
2025-11-27 21:28:31 +0530

    final

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -9,7 +9,7 @@ import close_icon from "./close_icon.svg"
 import users_icon from "./users_icon.svg"
 import car_icon from "./car_icon.svg"
 import location_icon from "./location_icon.svg";
-import addIcon from "./AddIcon.svg";
+import addIcon from "./addIcon.svg";
 import addIconColored from "./addIconColored.svg";
 import fuel_icon from "./fuel_icon.svg";
 import carIcon from "./carIcon.svg";
